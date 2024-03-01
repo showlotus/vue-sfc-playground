8a548499e1ac492c2881b0c0f08cee7ae172f754
showlotus <2257030399@qq.com>
2024-03-01 23:57:19 +0800

    release: update website

--- website/assets/wasm-VMJSURxR-B2s1jMdm.js
+++
@@ -1,7 +0,0 @@
-import{_ as n}from"./index-CO3ghUZt.js";const r=async e=>{const a=await n(()=>import("./onig-CJPi_5NU-BbrMXqi_.js"),__vite__mapDeps([])).then(t=>t.default);return WebAssembly.instantiate(a,e).then(t=>t.instance.exports)};export{r as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
